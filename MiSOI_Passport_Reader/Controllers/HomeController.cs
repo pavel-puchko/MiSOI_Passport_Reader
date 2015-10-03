3ad0f5ccc62b2f2836aab4ac1e1e7dbfffde7e3b
Pavel Puchko <thefearofkitty@gmail.com>
2015-10-03 20:19:59 +0300

    add upload image

--- MiSOI_Passport_Reader/Controllers/HomeController.cs
+++ MiSOI_Passport_Reader/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Services;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +10,34 @@ namespace MiSOI_Passport_Reader.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly BoxFilter _boxFilter;
+
+		public HomeController()
+		{
+			_boxFilter = new BoxFilter();
+		}
+
 		public ActionResult Index()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		public ActionResult UploadImage()
+		{
+			for (int i = 0; i < Request.Files.Count; i++)
+			{
+				HttpPostedFileBase file = Request.Files[i]; //Uploaded file
+															//Use the following properties to get file's name, size and MIMEType
+				int fileSize = file.ContentLength;
+				string fileName = file.FileName;
+				string mimeType = file.ContentType;
+				System.IO.Stream fileContent = file.InputStream;
+				Image outputImage = _boxFilter.Transform(new Bitmap(Image.FromStream(file.InputStream)), 15); // Box Filter
+
+				outputImage.Save(Server.MapPath("~/") + fileName); //File will be saved in application root
+			}
+			return Json("Uploaded " + Request.Files.Count + " files");
 		}
 
 		public ActionResult About()
